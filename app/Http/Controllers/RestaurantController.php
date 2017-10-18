8a2ba45a7ec70cebe75eeeb85268356d90114738
muhakh <muhakh2@gmail.com>
2017-10-18 18:00:07 +0200

    Created restaurants controller

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Restaurant;
 
 class RestaurantController extends Controller
 {
-    //
+    public function index()
+    {
+        return Restaurant::all();
+    }
+
+    public function display($id)
+    {
+        return Restaurant::find($id);
+    }
+
+    public function create(Request $request)
+    {
+        $restaurant = Restaurant::create($request->all());
+        return response()->json($article, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $restaurant = Restaurant::findOrFail($id);
+        $restaurant->update($request->all());
+
+        return response()->json($restaurant, 200);
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $restaurant = Restaurant::findOrFail($id);
+        $restaurant->delete();
+
+        return response()->json(null, 204);
+    }
 }
