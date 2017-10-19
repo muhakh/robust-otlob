289779cb7c3b44c1ea7f503d4914892b55d6501c
muhakh <muhakh2@gmail.com>
2017-10-19 08:16:01 +0200

    Support one to many relation for Restaurant and MenuItem

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model
 {
-    //
+    protected $fillable = ['name', 'manager_id'];
+
+    /**
+     * Get the menu items for the blog post.
+     */
+    public function menu_items()
+    {
+        return $this->hasMany('App\MenuItem');
+    }
 }
