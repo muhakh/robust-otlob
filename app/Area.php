2efe7ba3199bef4ae47193493a42396d447b2abf
muhakh <muhakh2@gmail.com>
2017-10-20 09:59:52 +0200

    Removed unnecessary function

--- app/Area.php
+++ app/Area.php
@@ -7,13 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Area extends Model
 {
     protected $fillable = ['name', 'city'];
-
-    /**
-     * The restaurants that belong to the area.
-     */
-    public function restaurants()
-    {
-        return $this->belongsToMany('App\Restaurant', 'restaurant_areas', 'restaurant_id', 'area_id')
-                    ->withTimestamps();
-    }
 }
