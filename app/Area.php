08439e430d405b96838f8f95a1c7397de2f6f0ad
muhakh <muhakh2@gmail.com>
2017-10-22 07:05:56 +0200

    Added relation between Restaurants and Areas

--- app/Area.php
+++ app/Area.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Area extends Model
 {
     protected $fillable = ['name', 'city'];
+
+    /**
+     * The restaurants that belong to the area.
+     */
+    public function restaurants()
+    {
+        return $this->belongsToMany('App\Restaurant', 'restaurant_areas', 'area_id', 'restaurant_id')
+                    ->withTimestamps();
+    }
 }
